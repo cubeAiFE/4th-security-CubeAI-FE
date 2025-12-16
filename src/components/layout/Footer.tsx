52855f6dfa0f31fb730545be8764ef4ab114b602
dongmin0204 <nm2205@dgu.ac.kr>
2025-12-17 02:39:13 +0900

    fix: 메인 페이지와 패딩 일치화

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import Logo from '@/assets/icons/logo_colored.png';
 export default function Footer() {
   return (
     <footer className="border-t bg-[#E5E5E5] text-black">
-      <div className="mx-auto max-w-7xl px-6 py-8 text-sm text-zinc-500 flex flex-col gap-[13px]">
+      <div className="mx-auto max-w-100% px-4 md:px-[153px] py-8 text-sm text-zinc-500 flex flex-col gap-[13px]">
         <img src={Logo} alt="Logo" className="w-20 h-auto object-contain" />
         <p>복잡한 딥러닝 모델 설계, 이제는 큐브 AI와 함께 하세요.</p>
       </div>
